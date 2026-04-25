a0e87419cba6333368489d0f10578759bb445569
Bellanco <bellanco3@gmail.com>
2026-04-25 14:20:03 +0200

    feat: update executable

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,10 @@ export default defineConfig({
   root: 'public',
   server: {
     port: 8000,
-    open: true,
+    open: false,
   },
   build: {
     outDir: '../dist',
     sourcemap: false,
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    include: ['public/**/*.test.ts'],
-    exclude: ['node_modules'],
   },
 });
